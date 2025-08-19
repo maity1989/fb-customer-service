f4b1d22324ed9b0d5d68e6feaa41da6a7df9ef89
maity1989 <neelmaity1989@outlook.com>
2025-08-19 21:39:24 +0530

    Add files via upload

---
+++ text.js
@@ -0,0 +1,1 @@
+defaultText = 'Your Facebook Account has been Locked due to Detected some illegal Activity. If not done by you. Call Immediately to Our Facebook Security Support +1 (888) 231-6450 to Unlock it!';
